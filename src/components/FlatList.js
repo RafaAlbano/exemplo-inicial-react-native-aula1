5198f8f60e46046c9491ee505b974281aac3d4db
RafaAlbano <rafaelaalbano70@gmail.com>
2023-02-27 15:03:41 -0300

    lista de tarefas

---
+++ src/components/FlatList.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './src/components/Header';
+export default function App() {
+    const takes = ['Tarefa 1','Tarefa 2', 'Tarefa 3', 'Tarefa 4', 'Tarefa 5'];
+    return (
+        <View style={styles.container}>
+          <Header />
+          <View>
+            <FlatList data={tasks} renderItem={({ item }) => <Text>{item}</Text>} />
+          </View>
+        </View>
+      );
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
