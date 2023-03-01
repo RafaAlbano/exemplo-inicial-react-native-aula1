6121e133db92d83a500d2cd5e491884a4c9eba38
RafaAlbano <rafaelaalbano70@gmsil.com>
2023-03-01 15:14:18 -0300

    ate o Header

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/Header'
 
 export default function App() {
+  const tasks = ['task 1','Task 2','Task3']
   return (
     <View style={styles.container}>
-      <Header />
-      <Text>Tarefa1</Text>
+      <Header titulo="lista de tarefas" />
+      <Header titulo="lista de alunos" />
+      <Header titulo="lista de professores" />
+      <View>
+         {tasks.map((task) => (
+          <Text>{task}</Text>
+        ))}
+      </View>
+      <StatusBar style="auto" />
     </View>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,23 @@
+import { Component } from 'react'; 
 import { Text, View, StyleSheet } from 'react-native';
-export default function Header() {
-  return (
-    <View style={styles.header}>
-      <Text style={styles.headerText}>Lista de Tarefas</Text>
-    </View>
-  );
+
+// export default function Header (props) {
+//   return (
+//     <View style={styles.header}>
+//       <Text>{props}</Text>
+//     </View>
+//   );
+// }
+export default class Header extends Component {
+  render () {
+    return (
+      <View style={styles.header}>
+        <Text >
+          {this.props.titulo}
+        </Text>  
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
